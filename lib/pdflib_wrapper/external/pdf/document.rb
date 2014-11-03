0b304ed5fa4c39f1ff37a004d2132ee4e768931c
Michael Hayes <tasslehawf@mac.com>
2014-11-03 17:31:36 -0600

    add fill_textblock method

--- lib/pdflib_wrapper/external/pdf/document.rb
+++ lib/pdflib_wrapper/external/pdf/document.rb
@@ -1,26 +1,36 @@
 module PdflibWrapper
-	module External
-		module Pdf
-			class Document
-				attr_accessor :document
-				def initialize(pdf, filepath, opts={})
-					#TODO: support opts
-					key_values = [:password]
-					singles = []
-					@pdf = pdf
-					@document = @pdf.open_pdi_document( filepath, OptionListMapper.create_options('', key_values, singles, opts) )
-				end
+  module External
+    module Pdf
+      class Document
+        attr_accessor :document
+        def initialize(pdf, filepath, opts={})
+          #TODO: support opts
+          key_values = [:password]
+          singles = []
+          @pdf = pdf
+          @document = @pdf.open_pdi_document( filepath, OptionListMapper.create_options('', key_values, singles, opts) )
+        end
 
-			  def open_page(page_number=1, opts={})
-			  	#TODO: support opts
-			  	External::Pdf::Page.new(@pdf, self, page_number, "" )
-			  end
+        def open_page(page_number=1, opts={})
+          #TODO: support opts
+          External::Pdf::Page.new(@pdf, self, page_number, "" )
+        end
 
-				def close
-					#TODO: support opts
-					@pdf.close_pdi_document(@document)
-				end
-			end
-		end
-	end
+        def fill_textblocks(page, data)
+          data.each do |key, value|
+            fill_textblock(page, key, value)
+          end
+        end
+
+        def fill_textblock(page, key, value)
+          @pdf.fill_textblock(page, key, value, "embedding encoding=unicode")
+        end
+
+        def close
+          #TODO: support opts
+          @pdf.close_pdi_document(@document)
+        end
+      end
+    end
+  end
 end
